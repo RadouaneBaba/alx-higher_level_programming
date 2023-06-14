8ecd9600dd0de31018289ba23c6ae6f8246267f4
RadouaneBaba <radoine.baba04@gmail.com>
2023-06-14 14:39:30 -0700

    final

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    
+
     r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    result = 0 
+    result = 0
     if type(roman_string) != str or not roman_string:
         return 0
 
