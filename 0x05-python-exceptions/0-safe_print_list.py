077c0e3a4dc173614290ec00051f15c43cd7db58
RadouaneBaba <radoine.baba04@gmail.com>
2023-06-26 16:31:15 -0700

    test

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,11 @@
 def safe_print_list(my_list=[], x=0):
     n = 0
     try:
-        for n in range(x):
-            print(my_list[n], end="")
+        for elem in my_list:
+            print(elem, end="")
+            n += 1
         print("")
     except ValueError:
         print("Error")
+    finally:
+        return n
