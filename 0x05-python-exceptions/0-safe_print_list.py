1fe17dd55726e7d8648c3dbcf90d0451f1d05d8f
RadouaneBaba <radoine.baba04@gmail.com>
2023-06-26 16:39:26 -0700

    test

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,6 +4,8 @@ def safe_print_list(my_list=[], x=0):
     n = 0
     try:
         for elem in my_list:
+            if n >= x:
+                break
             print(elem, end="")
             n += 1
         print("")
