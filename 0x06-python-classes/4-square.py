d3ace0341d3014933d059ad9b3829431629a3f28
RadouaneBaba <radoine.baba04@gmail.com>
2023-06-27 17:58:53 -0700

    fifth

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -29,3 +29,16 @@ class Square:
             area of square
         """
         return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
