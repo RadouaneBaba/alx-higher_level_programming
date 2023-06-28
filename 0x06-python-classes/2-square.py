4cde1d4f416bc2e5346f81c58bb444a775f8ca09
RadouaneBaba <radoine.baba04@gmail.com>
2023-06-27 17:29:02 -0700

    third

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+    define a square class
+Classes:
+    Square
+"""
+
+
+class Square:
+    """ square class definition """
+    def __init__(self, size=0):
+        """
+            initialise square object
+        Args:
+            size (int): size of square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
