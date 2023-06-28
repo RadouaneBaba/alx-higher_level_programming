023ddb9431344743a8a9d932e4d9f37cb7347e7e
RadouaneBaba <radoine.baba04@gmail.com>
2023-06-27 18:13:57 -0700

    lst

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+"""
+    define a square class
+Classes:
+    Square
+"""
+
+
+class Square:
+    """ square class definition """
+    def __init__(self, size=0):
+        """
+            initialise square object
+        Args:
+            size (int): size of square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        get square area
+        Returns:
+            area of square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        get size
+        Returns:
+            size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Set size
+        Args:
+            value (int): value for size
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """ print square """
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
