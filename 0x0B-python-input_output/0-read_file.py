859101338792957a9ecd5fcfcf6f4d32bcf3874e
RadouaneBaba <radoine.baba04@gmail.com>
2023-07-11 14:09:04 -0700

    first

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" define a fnction that reads a text file and print it """
+
+
+def read_file(filename=""):
+    with open(filename, encoding="utf-8") as my_file:
+        print(my_file)
