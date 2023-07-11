2f665294835585fe207ebc4061adac43ef06daf3
RadouaneBaba <radoine.baba04@gmail.com>
2023-07-11 14:59:33 -0700

    third

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,9 +5,6 @@
 
 def write_file(filename="", text=""):
     """ write to a file """
-    with open(filename, 'a', encoding="utf-8") as f:
-        n = 0
-        for i in text:
-            f.write(i)
-            n += 1
-        return n
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(text)
+        return len(text)
---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" append to a file """
+
+def append_write(filename="", text=""):
+    """ function """
+    with open(filename, 'a', encoding="utf-8") as f:
+        f.write(text)
+        return len(text)
