3f336da6cfcff397b79eeb4e81680284eb7d3510
RadouaneBaba <radoine.baba04@gmail.com>
2023-07-12 10:54:07 -0700

    ten

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" module """
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if not attrs:
+            return self.__dict__
+        my_obj = {}
+        for s in my_obj:
+            if s in self.__dict__.keys():
+                my_obj[s] = self.__dict__[s]
