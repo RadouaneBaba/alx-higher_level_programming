711e4f349467bc95c3a8eb1f708134332aad8e08
Lsblackk <radoine.baba04@gmail.com>
2023-09-12 23:50:36 +0100

    Delete 0x0B-python-input_output directory
    alx defer

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-""" define a fnction that reads a text file and print it """
-
-
-def read_file(filename=""):
-    """ func """
-    with open(filename, encoding="utf-8") as my_file:
-        print(my_file.read(), end="")
--- 0x0B-python-input_output/8-class_to_json.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-""" module """
-
-
-def class_to_json(obj):
-    """ functoinn """
-    return obj.__dict__
