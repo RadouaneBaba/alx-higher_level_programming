1ad16490b506eeefe08d11c4cd7c10eee9223235
RadouaneBaba <radoine.baba04@gmail.com>
2023-10-09 11:22:04 -0700

    fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,6 +3,6 @@
 """ inherits from mod """
 
 
-def inheris_from(obj, a_class):
+def inherits_from(obj, a_class):
     """ check if object inherited from class """
     return issubclass(type(obj), a_class)
