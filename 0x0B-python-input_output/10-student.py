04539c7dd9527027491b9f97e139d82cd92465a7
RadouaneBaba <radoine.baba04@gmail.com>
2023-10-10 15:27:04 -0700

    chekc

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+""" module of student class """
+
+
+class Student:
+    """ define student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) == list:
+            for attr in attrs:
+                if type(attr) != str:
+                    return self.__dict__
+            f_dict = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    f_dict[attr] = self.__dict__[attr]
+            return f_dict
+        return self.__dict__
