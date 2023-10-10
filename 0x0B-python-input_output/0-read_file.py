b3ba44c132e9fbd2e0631040ab0bd27a1b4e8ea2
RadouaneBaba <radoine.baba04@gmail.com>
2023-10-10 14:12:29 -0700

    fix one

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,9 +5,6 @@
 
 def read_file(filename=""):
     """ func that reads a file """
-    try:
-        with open(filename, 'r', encoding="utf-8") as f:
-            data = f.read()
-            print(data)
-    except Exception as e:
-        print(e)
+    with open(filename, 'r', encoding="utf-8") as f:
+        data = f.read()
+        print(data)
