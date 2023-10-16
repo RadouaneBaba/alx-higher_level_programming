6fedd948da45e4f31b7994c75d46b5c1126428a8
RadouaneBaba <radoine.baba04@gmail.com>
2023-10-16 13:28:24 -0700

    last

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,21 +91,28 @@ class Rectangle(Base):
         s = f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"
         return f"{s} - {self.__width}/{self.__height}"
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """ update method """
-        if args is None:
-            return
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.integer_validator("width", args[1])
-            self.__width = args[1]
-        if len(args) >= 3:
-            self.integer_validator("height", args[2])
-            self.__height = args[2]
-        if len(args) >= 4:
-            self.integer_validator("x", args[3])
-            self.__x = args[3]
-        if len(args) >= 5:
-            self.integer_validator("y", args[4])
-            self.__y = args[4]
+        if args is not None:
+            if len(args) >= 1:
+                super().__init__(args[0])
+            if len(args) >= 2:
+                self.integer_validator("width", args[1])
+                self.width = args[1]
+            if len(args) >= 3:
+                self.integer_validator("height", args[2])
+                self.height = args[2]
+            if len(args) >= 4:
+                self.integer_validator("x", args[3])
+                self.x = args[3]
+            if len(args) >= 5:
+                self.integer_validator("y", args[4])
+                self.y = args[4]
+
+        if not args and kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "id":
+                    super().__init__(value)
+                else:
+                    self.integer_validator(key, value)
+                    setattr(self, key, value)
