7664221fae4cfb7110742f5228cb73a930a40b36
RadouaneBaba <radoine.baba04@gmail.com>
2023-10-16 11:22:55 -0700

    fix

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" test for rectangle"""
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """ test class rectangle """
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
