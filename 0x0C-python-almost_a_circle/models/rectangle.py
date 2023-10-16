fb480e1df35e4316cffb39387f03e425455d662a
RadouaneBaba <radoine.baba04@gmail.com>
2023-10-16 12:37:42 -0700

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,3 +90,20 @@ class Rectangle(Base):
         """ method doc """
         s = f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"
         return f"{s} - {self.__width}/{self.__height}"
+
+    def update(self, *args):
+        """ update method """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.integer_validator("width", args[1])
+            self.__width = args[1]
+        if len(args) >= 3:
+            self.integer_validator("height", args[2])
+            self.__height = args[2]
+        if len(args) >= 4:
+            self.integer_validator("x", args[3])
+            self.__x = args[3]
+        if len(args) >= 5:
+            self.integer_validator("y", args[4])
+            self.__y = args[4]
