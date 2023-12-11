74ff4b142d247c188166cb2a698e0b6b16fc92fc
RadouaneBaba <radoine.baba04@gmail.com>
2023-12-11 14:54:07 -0800

    eleven

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,10 +7,22 @@ if (argv.length === 2 || argv.length === 3) {
 } else {
   let biggest = parseInt(argv[2]);
   let secBiggest = parseInt(argv[2]);
+  let index = 2;
   for (let i = 3; i < argv.length; i++) {
     if (parseInt(argv[i]) > biggest) {
-      secBiggest = biggest;
       biggest = parseInt(argv[i]);
+      index = i;
+    }
+  }
+  if (secBiggest === biggest) {
+    secBiggest = parseInt(argv[3]);
+  }
+  for (let i = 3; i < argv.length; i++) {
+    if (i == index) {
+      continue;
+    }
+    if (parseInt(argv[i]) > secBiggest) {
+      secBiggest = parseInt(argv[i]);
     }
   }
   console.log(secBiggest);
