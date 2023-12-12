78a6cfc4c7612dc24646ada69054aaaeae0cc4b9
RadouaneBaba <radoine.baba04@gmail.com>
2023-12-12 11:19:42 -0800

    final

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,10 +2,10 @@
 
 exports.esrever = function (list) {
   let temp;
-  for (let i = 0; i < (list.length / 2); i++){
+  for (let i = 0; i < (list.length / 2); i++) {
     temp = list[i];
-    list[i] = list[list.length - i];
-    list[list.length - i] = temp;
+    list[i] = list[list.length - i - 1];
+    list[list.length - i - 1] = temp;
   }
   return list;
 };
