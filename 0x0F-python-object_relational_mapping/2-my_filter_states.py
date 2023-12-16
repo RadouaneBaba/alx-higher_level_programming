53ec5b24d26e1534502c49a325532a1de253691a
RadouaneBaba <radoine.baba04@gmail.com>
2023-12-16 02:26:25 -0800

    userinput

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""displays all values in the states where name matches the arguments"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM state WHERE name={};".format(argv[4]))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
