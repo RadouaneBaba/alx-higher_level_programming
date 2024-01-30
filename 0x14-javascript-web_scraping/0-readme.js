2527693cc437472d4e1fc7caa7eba48d72c72d14
RadouaneBaba <radoine.baba04@gmail.com>
2024-01-30 15:46:00 -0800

    fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-const fs = require('node:fs');
+const fs = require('fs');
 
-fs.readFile(argv[2], 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
