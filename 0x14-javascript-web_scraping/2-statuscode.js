856734c0d79aa36a5a58eea67fa18e4c371ebec2
RadouaneBaba <radoine.baba04@gmail.com>
2024-01-30 16:17:33 -0800

    star wars

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,5 @@
 const request = require('request');
 request(process.argv[2], (err, res, body) => {
   if (err) return;
-  console.log('code: ', res.statusCode);
+  console.log('code:', res.statusCode);
 });
---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const request = require('request');
+const myPath = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(myPath, {json: true}, (err, res, body) => {
+  if (err) return;
+  console.log(body.title);
+});
