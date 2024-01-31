047115f3c833d7d36bb57d1047c06aa69096190f
RadouaneBaba <radoine.baba04@gmail.com>
2024-01-30 16:28:41 -0800

    count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+const chaUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+const options = { json: true };
+
+request(url, options, (err, res, body) => {
+  if (err) return;
+  const myArr = body.results.filter(a => a.characters.includes(chaUrl));
+  console.log(myArr.length);
+});
